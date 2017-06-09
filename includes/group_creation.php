cfedbd64a75648359176078b092399682e0fcc03
corsac <corsac@hushmail.com>
2017-06-09 12:25:06 +0100

    Group Creation: add token to register link to disable random group joining

--- includes/group_creation.php
+++ includes/group_creation.php
@@ -24,30 +24,51 @@
 if (!session_id()) {
     session_start();
 }
-if (isset($_GET["zume-group-id"])){
-	$_SESSION["zume_group_id"] = $_GET["zume-group-id"];
-	setcookie("zume_group_id", $_GET["zume-group-id"], time()+86400*7);
+if (isset($_GET["group-id"]) && isset($_GET["zgt"])){
+	$_SESSION["zume_group_id"] = $_GET["group-id"];
+	$_SESSION["zume_group_token"] = $_GET["zgt"];
 }
-if (isset($_SESSION["zume_group_id"])){
+
+
+
+if (isset($_SESSION["zume_group_id"]) && isset($_SESSION["zume_group_token"])){
 	$group = groups_get_group((int)$_SESSION["zume_group_id"]);
-	$_SESSION["group"] = $group;
-	if (!groups_is_user_member(get_current_user_id(), $_SESSION["zume_group_id"]) && $group->creator_id){
-		$_SESSION["test"] = groups_invite_user(array(
-			"user_id"=>get_current_user_id(),
-			"group_id" => $_SESSION["zume_group_id"],
-			"inviter_id" => $group->creator_id,
-			"is_confirmed" => true
-			)
-		);
-		if (groups_accept_invite(get_current_user_id(), $_SESSION["zume_group_id"])){
-			$_SESSION["zume_group_id"] = "";
-		};
+	$group_token = groups_get_groupmeta($group->id, "group_token");
+	if (isset($group_token) && $group_token == $_SESSION["zume_group_token"]){
+		if (!groups_is_user_member(get_current_user_id(), $_SESSION["zume_group_id"]) && $group->creator_id){
+			$_SESSION["test"] = groups_invite_user(array(
+				"user_id"=>get_current_user_id(),
+				"group_id" => $_SESSION["zume_group_id"],
+				"inviter_id" => $group->creator_id,
+				"is_confirmed" => true
+				)
+			);
+			if (groups_accept_invite(get_current_user_id(), $_SESSION["zume_group_id"])){
+				$_SESSION["zume_group_id"] = "";
+				$_SESSION["zume_group_token"] = "";
+			};
+		}
 	}
 }
 
-function set_default_privacy($group_id){
-	groups_edit_group_settings($group_id, false, "private", "admins");
+function generate_string($length) {
+
+	$chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+	$string = "";
+
+        for ($i = 0; $i < $length; $i++) {
+	  $string .= $chars[mt_rand(0, strlen($chars) - 1)];
+	}
+
+	return $string;
 }
 
-add_action( 'groups_created_group',  'set_default_privacy' );
+function set_group_defaults($group_id){
+	groups_edit_group_settings($group_id, false, "private", "admins");
+	if (!groups_get_groupmeta($group_id, "group_token")){
+		$token = generate_string(10);
+		groups_update_groupmeta($group_id, "group_token",$token);
+	}
+}
 
+add_action( 'groups_created_group',  'set_group_defaults' );
