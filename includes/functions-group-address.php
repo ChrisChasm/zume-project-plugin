d1d7fd50a1ee2bdf5db5ceda2aa9f16a69da40cc
Chris Wynn <chris@chasm.solutions>
2017-06-12 12:09:43 -0600

    New edit

--- includes/functions-group-address.php
+++ includes/functions-group-address.php
@@ -45,17 +45,78 @@ function group_create_fields_markup() {
     global $bp, $wpdb;
     ?>
 
-    <label for="address">Address (required)</label>
-    <input id="address" type="text" name="address" value="" required/>
+    <label for="address">Search with your address for your tract in the map below.</label>
+    <input id="address" type="text" name="address" value="" required/> <button class="button" type="button" value="submit">Search</button>
 
-    <label for="city">City (required)</label>
-    <input id="city" type="text" name="city" value="" required/>
+    <input type="hidden" name="tract" />
 
-    <label for="state">State (required)</label>
-    <input id="state" type="text" name="state" value="" required/>
+    <style>
+        /* Always set the map height explicitly to define the size of the div
+    * element that contains the map. */
+        #map {
+            height: 600px;
+            width: 75%;
+        }
+        /* Optional: Makes the sample page fill the window. */
+        html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+    <div id="map" style="height:200px;"></div>
 
-    <label for="zip">Zip (required)</label>
-    <input id="zip" type="text" name="zip" value="" required/>
+    <script type="text/javascript">
+
+        jQuery(document).ready(function() {
+            var map;
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 38.7767479, lng: -104.0954098},
+                zoom: 3
+            });
+
+            jQuery('button').click( function () {
+                jQuery
+
+                var address = jQuery('#address').val();
+                var restURL = '<?php echo get_rest_url(null, '/lookup/v1/tract/gettractmap'); ?>';
+                jQuery.post( restURL, { address: address })
+                    .done(function( data ) {
+                        jQuery('#search-response').html('We found that your tract is ' + data.geoid );
+
+                        jQuery('#map').css('height', '600px');
+
+                        var map = new google.maps.Map(document.getElementById('map'), {
+                            zoom: data.zoom,
+                            center: {lng: data.lng, lat: data.lat},
+                            mapTypeId: 'terrain'
+                        });
+
+                        // Define the LatLng coordinates for the polygon's path.
+                        var coords = [ data.coordinates ];
+
+                        var tracts = [];
+
+                        for (i = 0; i < coords.length; i++) {
+                            tracts.push(new google.maps.Polygon({
+                                paths: coords[i],
+                                strokeColor: '#FF0000',
+                                strokeOpacity: 0.5,
+                                strokeWeight: 2,
+                                fillColor: '',
+                                fillOpacity: 0.2
+                            }));
+
+                            tracts[i].setMap(map);
+                        }
+
+                    });
+            });
+        });
+    </script>
+    <script
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcddCscCo-Uyfa3HJQVe0JdBaMCORA9eY">
+    </script>
 
     <?php
 }
@@ -83,40 +144,3 @@ add_filter( 'bp_after_group_details_creation_step', 'group_create_fields_markup'
 add_filter( 'bp_after_group_details_admin', 'group_edit_fields_markup' );
 add_action( 'groups_group_details_edited', 'group_header_fields_save' );
 add_action( 'groups_created_group',  'group_header_fields_save' );
-
-
-
-
-
-
-
-
-
-// TODO: NOTES FOR FUTURE USE
-
-// Using wp-content/plugins/buddypress/bp-templates/bp-legacy/buddypress/groups/create.php
-// And using wp-content/plugins/buddypress/bp-themes/bp-default/groups/create.php
-
-
-//add_action('bp_after_group_invites_creation_step', 'zume_after_invitation_explaination');
-//add_action('bp_before_group_invites_creation_step', 'zume_before_invitation_explaination');
-//
-//function zume_before_invitation_explaination () {
-//    print '<p>This is a Zúme explaination before the invitation process.</p>';
-//}
-//
-//function zume_after_invitation_explaination () {
-//    print 'This is a Zúme explaination after the invitation process.';
-//}
-
-//function zume_after_group_description () {
-//    print '<label>Address (required)</label><input type="text" name="group_address" required/>';
-//    print '<label>City (required)</label><input type="text" name="group_city" required/>';
-//    print '<label>State (required)</label><input type="text" name="group_state" required/>';
-//    print '<label>Zip (required)</label><input type="text" name="group_zip" required/>';
-//}
-//add_action('bp_after_group_details_creation_step', 'zume_after_group_description');
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
