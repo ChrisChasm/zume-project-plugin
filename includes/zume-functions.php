59a65956ecd1a2d62cf2aa40a2083f5bef4b7e47
Chris Wynn <chris@chasm.solutions>
2017-06-13 11:35:27 -0600

    Password reset

--- includes/zume-functions.php
+++ includes/zume-functions.php
@@ -20,11 +20,12 @@ function zume_force_login() {
         'Resources'
     );
 
-    if(is_page($exception_pages) || $GLOBALS['pagenow'] === 'wp-login.php'  ) {
+    if(is_page($exception_pages) || $GLOBALS['pagenow'] === 'wp-login.php' || (! empty( $_REQUEST['action'] ) && $_REQUEST['action'] === 'rp')  ) {
         return;
     }
 
-    // If using BuddyPress and on the register/activate page, don't do anything.
+
+
     if ( function_exists( 'bp_is_current_component' ) && ( bp_is_current_component( 'register' ) || bp_is_current_component( 'activate' ) ) )
         return;
 
