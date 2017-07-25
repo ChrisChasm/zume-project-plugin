3630311bff1290393631525e7e326aadefcddd1d
Chris Wynn <chris@chasm.solutions>
2017-07-25 13:13:08 -0600

    modify admin for coach

--- includes/zume-functions.php
+++ includes/zume-functions.php
@@ -250,17 +250,22 @@ add_filter('bp_get_send_public_message_button', '__return_false');
  * Remove menu items for coaches in the admin dashboard.
  */
 function custom_menu_page_removing() {
-    remove_menu_page( 'index.php' );                  //Dashboard
-    remove_menu_page( 'jetpack' );                    //Jetpack*
-    remove_menu_page( 'edit.php' );                   //Posts
-    remove_menu_page( 'upload.php' );                 //Media
-    remove_menu_page( 'edit.php?post_type=page' );    //Pages
-    remove_menu_page( 'edit.php?post_type=steplog' );    //Pages
-    remove_menu_page( 'edit-comments.php' );          //Comments
-    remove_menu_page( 'themes.php' );                 //Appearance
-    remove_menu_page( 'plugins.php' );                //Plugins
-//    remove_menu_page( 'users.php' );                  //Users
-    remove_menu_page( 'tools.php' );                  //Tools
-    remove_menu_page( 'options-general.php' );        //Settings
+
+    if(is_admin() && current_user_can('coach')) {
+
+        remove_menu_page( 'index.php' );                  //Dashboard
+        remove_menu_page( 'jetpack' );                    //Jetpack*
+        remove_menu_page( 'edit.php' );                   //Posts
+        remove_menu_page( 'upload.php' );                 //Media
+        remove_menu_page( 'edit.php?post_type=page' );    //Pages
+        remove_menu_page( 'edit.php?post_type=steplog' );    //Pages
+        remove_menu_page( 'edit-comments.php' );          //Comments
+        remove_menu_page( 'themes.php' );                 //Appearance
+        remove_menu_page( 'plugins.php' );                //Plugins
+    //    remove_menu_page( 'users.php' );                  //Users
+        remove_menu_page( 'tools.php' );                  //Tools
+        remove_menu_page( 'options-general.php' );        //Settings
+        
+    }
 }
 add_action( 'admin_menu', 'custom_menu_page_removing' );
